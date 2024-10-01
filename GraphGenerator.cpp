87e984c2103c955f6a27325b53b2121f2c963fa3
Bartosz Michalak <michalak.bartosz67@gmail.com>
2024-10-01 23:34:58 +0200

    Simple graph gen already working

--- GraphGenerator.cpp
+++ GraphGenerator.cpp
@@ -1,72 +1,78 @@
-/*
-  FILE TO BE REVIEWED 
-
-  potential multithreading in the future
-*/
-
 #include <cstdint>
-#include <optional> //learn how to use it 
-#include <list> //array?
-
-struct NeighbourhoodList
-{
-  //matrix?
-  //list of nodes? ||
-  //list of edges? ||
-  
-};
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <random> 
 
 struct Node
 {
   uint16_t id; // think how many we may have 
   bool visited; //potentially optional
 
-
-  NeighbourhoodList* neighbours; // potentially many implemenatitions 
-  // one global instance? I think so
-
-
   Node(uint16_t id)
   {
     this->id = id;
     visited = false;
-
   };
   ~Node(){};
+
+  operator int() const {
+    return (int)id;
+  }
+  operator uint16_t() const {
+    return id;
+  }
 };
 
 class Graph {
+  private:
+    uint16_t numberOfNodes;
+    uint16_t numberOfEdges;
+
+    //adjacency list - table of vectors (vec of vecs if we allow adding new Nodes)
+    std::vector<Node>* adjacencyList;
+
   public:
     Graph(uint16_t numberOfNodes, uint16_t numberOfEdges)
     {
       this->numberOfNodes = numberOfNodes;
       this->numberOfEdges = numberOfEdges;
-      listOfNodes = new Node[numberOfNodes]; //is this correct?
+      this->adjacencyList = new std::vector<Node>[numberOfNodes];
     };
-    
-    ~Graph()
+    ~Graph(){};
+    void addEdge(Node node1, Node node2)
     {
-      delete[] listOfNodes;
-    };
-
-    void switchRepresentation();
-    void addNode(uint16_t position, uint16_t id)
-    {
-      listOfNodes[position].id = id;
+      // std::cout << "adding edge between " << (uint16_t) node1 << " and " << (uint16_t) node2 << std::endl;
+      this->adjacencyList[(uint16_t)node1].push_back(node2);
     }
-    //void printData(); // representation? numbers etc
-
-  private:
-    uint16_t numberOfNodes;
-    uint16_t numberOfEdges;
-
-    Node* listOfNodes;
-    
-    //list of nodes
-    //list of edges?
-
+    //rm edge/vertex
+    void printData()  // mozna ladniej np wszystkie po 3/4 cyfry itd
+    {
+      for (int i = 0; i < numberOfNodes; ++i)
+      {
+        std::cout << "node "<< i << "\n\t"; 
+        for (int j = 0; j < adjacencyList[i].size(); ++j)
+        {
+          std::cout <<  (uint16_t) adjacencyList[i].at(j) << " "; 
+        }
+        std::cout << std::endl;
+      }
+    }
 };
 
+class Random {
+  private:
+    std::random_device rd;
+    std::mt19937 generator;
+  public:
+    Random() : generator(rd()){};
+    ~Random(){};
+    uint16_t generateRandomNumber(uint16_t max)
+    {
+      return std::uniform_int_distribution<uint16_t> {0, max}(this->generator);
+    }
+};
 
 // Mozemy podac graph jako ref a graph tworzymy gdzie indziej
 void generateGraph(
@@ -75,20 +81,77 @@ void generateGraph(
     float denisty)        //potentially more params
 {
   Graph graph = Graph(numberOfNodes, numberOfEdges);
-  for (uint16_t i = 0; i < numberOfNodes; ++i) 
-  {
-    graph.listOfNodes[i] = Node(i); // given IDs to nodes is pointless, we may do it at init, here we add edges
-  }
-
+  Random randomGen;
+  uint16_t range = numberOfNodes - 1;// std::max({numberOfNodes,numberOfEdges});
   for (uint16_t i = 0; i < numberOfEdges; ++i) 
   {
     /*
     Simple idae: random 2 nodes, connect, repeart till target is met
-    */
-    uint16_t random1;
-    uint16_t random2;
+    */ 
+    graph.addEdge( Node(randomGen.generateRandomNumber(range)), Node(randomGen.generateRandomNumber(range)) );
     //add edge 1-2 2-1 bc biderectional 
     // update neighourhood list?
   }
+  graph.printData();
 }
 
+void writeOutput()
+{
+
+}
+
+void parseInput(
+    int argc,
+    char *argv[],
+    uint16_t &numberOfNodes, 
+    uint16_t &numberOfEdges,
+    uint16_t &state)
+{
+  switch(argc) 
+  {
+    case 1: 
+      std::cerr << "No arguments" << std::endl;
+      state = 1;
+      break;
+    default:
+      numberOfNodes = (uint16_t) std::stoi(argv[1]); 
+      numberOfEdges = (uint16_t) std::stoi(argv[2]); 
+      /*
+      ...
+      */
+      state = 0;
+      break;
+  }
+}
+
+/*
+Usage: numberOfNodes numberOfEdges
+*/
+int main(int argc, char *argv[]) 
+{
+  uint16_t numberOfNodes, numberOfEdges, state;
+
+  //tmp solution
+  float denisty = 0.1;
+
+  parseInput(argc, argv, numberOfNodes, numberOfEdges, state);
+  if(state) 
+  {
+    std::cerr << "Returning state = " << state << std::endl;
+    return state;
+  }
+  generateGraph(numberOfNodes, numberOfEdges, denisty);
+  writeOutput();
+  return 0;
+}
+
+
+/*
+  TODO: 
+    OSOBNE PLIKI
+    PRZYNAJMNIEJ MAKEFILE
+    jakies paramy grafu - zarowno genereowanie jak i liczenie post fatkum
+    notki?
+    rysowanie grafu?
+    algo jeszcze dodac
+*/
