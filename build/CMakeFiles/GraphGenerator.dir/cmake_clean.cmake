6e11e2f4d7f60bf36fc9d7a32012c32fdaa431e3
Bartosz Michalak <michalak.bartosz67@gmail.com>
2024-10-02 18:58:04 +0200

    Split into separate files

---
+++ build/CMakeFiles/GraphGenerator.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphGenerator.dir/src/graph.cpp.o"
+  "CMakeFiles/GraphGenerator.dir/src/graph.cpp.o.d"
+  "CMakeFiles/GraphGenerator.dir/src/main.cpp.o"
+  "CMakeFiles/GraphGenerator.dir/src/main.cpp.o.d"
+  "CMakeFiles/GraphGenerator.dir/src/random.cpp.o"
+  "CMakeFiles/GraphGenerator.dir/src/random.cpp.o.d"
+  "CMakeFiles/GraphGenerator.dir/src/utils.cpp.o"
+  "CMakeFiles/GraphGenerator.dir/src/utils.cpp.o.d"
+  "GraphGenerator"
+  "GraphGenerator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GraphGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
